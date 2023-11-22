a29a742d6235379b8b281f7a2f9c26547219904a
Chen <s1yuan_chen@163.com>
2023-11-22 16:33:00 +0800

    update

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.example.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,8 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //登录接口和注册接口不应拦截
-        registry.addInterceptor(loginInterceptor)
-                .excludePathPatterns("/user/login", "user/register");
+        //注册拦截器
+        InterceptorRegistration registration = registry.addInterceptor(loginInterceptor);
+        //所有路径都被拦截
+        registration.addPathPatterns("/**");
+        //添加不拦截路径
+        registration.excludePathPatterns("/**/user/login", "/**/user/register");
     }
 }
